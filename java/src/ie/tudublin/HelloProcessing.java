7abe1fb282f68996bdc49c5e9d786e384875d50a
Joshua <floresjoshua2603@gmail.com>
2023-02-01 11:06:49 +0000

    all seeing eye

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -11,8 +11,8 @@ public class HelloProcessing extends PApplet
 	}
 
 	public void setup() {
-		colorMode(HSB);
-		background(0);
+		//colorMode(HSB);
+		background(255,0,0);
 
 		
 		
@@ -20,6 +20,34 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
+		noStroke();
+		fill(255,255,0);
+		circle(250,250, 400);
+		fill(0,255,255);
+		triangle(250,10,50,400,450,400);
+		fill(255,255,255);
+		ellipse(250, 200, 180, 100);
+		fill(0,0,0);
+		ellipse(250,200,60,70);
+
+		ellipse(250,350,200,mouseY);
+
+		
+
+		/* 
+		stroke(255);
+		line(10,10,100,100); // x1, y1, x2, y2
+		circle(300, 250, 70); // cx, cy, d
+		fill(0,255,0);
+		rect(10, 300, 20, 100); // tlx, tly, w, h
+		stroke(127);
+		fill(255,255,0);
+		noStroke();
+		noFill();
+		strokeWeight(1);
+		triangle(40, 90, 300, 20, 80, 70);
+		fill(0,0,255);
+		*/
 		
 	}
 }
