2cc8821779182e630a4dda92612fbc905b8372c5
Joshua <floresjoshua2603@gmail.com>
2023-03-06 22:20:32 +0000

    Making the Game of Life (Incomplete)

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -4,6 +4,8 @@ import processing.core.PApplet;
 
 public class Life extends PApplet
 {
+	LifeBoard board;
+	LifeBoard next;
 
 	public void settings()
 	{
@@ -13,15 +15,19 @@ public class Life extends PApplet
 	public void setup() {
 		colorMode(HSB);
 		background(0);
-
 		
-		
+		board = new LifeBoard(100, this);
+		board.randomise();
+		next = new LifeBoard(100, this);
 	}
 
 	
 	
 	public void draw()
 	{	
-		
+		background(0);
+		board.render();
+		board.applyRules();
+
 	}
 }
